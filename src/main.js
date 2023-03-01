befd401f38084de9b2fc1309b3e13d5478ed5619
wr <2119495210@qq.com>
2023-03-01 20:09:47 +0800

    p012已完成

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+//注册三级联动--全局组件
+import TypeNav from '@/pages/Home/TypeNav'
+//第一个参数：全局组件名字，第二个参数：哪一个组件的
+Vue.component(TypeNav.name,TypeNav)
 
 Vue.config.productionTip = false
 
--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -1,5 +1,7 @@
 <template>
-  <div>我是首页
+  <div>
+    <!--三级联动全局组件已经注册为全局组件，所以不需要载引入-->
+    <TypeNav/>
   </div>
 </template>
 
